ceaf2aafda1e7cb6f245fe86f6a6fd000c4b9dbb
KiAi <94138724+broccoli88@users.noreply.github.com>
2023-04-19 20:02:31 +0200

    Main page grid mobile ver

--- js/script.js
+++ js/script.js
@@ -1,18 +1,35 @@
 // Nav
 
-const menu = document.querySelector(`.nav__menu`);
-const nav = document.querySelector(`.nav`);
+// const menu = document.querySelector(`.nav__menu`);
+// const nav = document.querySelector(`.nav`);
 
-menu.addEventListener(`click`, () => {
-	nav.classList.toggle(`collapsible`);
+// menu.addEventListener(`click`, () => {
+// 	nav.classList.toggle(`collapsible`);
+// });
+
+// window.addEventListener(`resize`, () => {
+// 	if (window.innerWidth >= 768) {
+// 		nav.classList.remove(`collapsible`);
+// 	}
+// });
+
+// Grid main
+
+const images = [
+	{ img: "../images/photos-main/banan.jpg" },
+	{ img: "../images/photos-main/biznes.jpg" },
+	{ img: "../images/photos-main/czekit.jpg" },
+	{ img: "../images/photos-main/dentify.jpg" },
+	{ img: "../images/photos-main/falk.jpg" },
+	{ img: "../images/photos-main/kia.jpg" },
+	{ img: "../images/photos-main/mercedes.jpg" },
+	{ img: "../images/photos-main/myclinic.jpg" },
+];
+const gridTiles = document.querySelectorAll(".grid__link");
+
+window.addEventListener(`load`, () => {
+	gridTiles.forEach((tile, index) => {
+		image = images[index].img;
+		tile.style.backgroundImage = `url(${image})`;
+	});
 });
-
-window.addEventListener(`resize`, () => {
-	if (window.innerWidth >= 768) {
-		nav.classList.remove(`collapsible`);
-	}
-});
-
-// Sub-nav
-
-const subNav = document.querySelector(`.sub-list-portfolio`);
