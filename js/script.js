2cbb2af387a1cbca4a6ad712806452972063522a
KiAi <94138724+broccoli88@users.noreply.github.com>
2023-04-21 20:11:11 +0200

    grid slide / hero fade with observer

--- js/script.js
+++ js/script.js
@@ -21,8 +21,8 @@ const images = [
 	{ img: "../images/photos-main/dentify.jpg" },
 	{ img: "../images/photos-main/falk.jpg" },
 	{ img: "../images/photos-main/biznes.jpg" },
-	{ img: "../images/photos-main/kia.jpg" },
 	{ img: "../images/photos-main/mercedes.jpg" },
+	{ img: "../images/photos-main/kia.jpg" },
 	{ img: "../images/photos-main/myclinic.jpg" },
 ];
 const gridTiles = document.querySelectorAll(".grid__link");
@@ -33,3 +33,25 @@ window.addEventListener(`load`, () => {
 		tile.style.backgroundImage = `url(${image})`;
 	});
 });
+
+// Grid slide
+
+const hero = document.querySelector(`.hero`);
+
+const gridObserver = new IntersectionObserver(
+	(entries) => {
+		const grid = document.querySelector(`.grid`);
+		if (!entries[0].isIntersecting) {
+			grid.classList.add(`grid-slide`);
+			hero.classList.add(`hero-fade`);
+		} else {
+			grid.classList.remove(`grid-slide`);
+			hero.classList.remove(`hero-fade`);
+		}
+	},
+	{
+		threshold: 0.98,
+	}
+);
+
+gridObserver.observe(hero);
